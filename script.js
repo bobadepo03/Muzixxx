b389d871500c1fdc28d35f792f3c187ff5944591
bobadepo03 <bobadepo03@gmail.com>
2026-05-23 16:35:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const music = document.getElementById("music");
+
+function playMusic(){
+  music.play();
+}
+
+function pauseMusic(){
+  music.pause();
+}
+
+function prevSong(){
+  alert("Previous Song");
+}
+
+function nextSong(){
+  alert("Next Song");
+}
